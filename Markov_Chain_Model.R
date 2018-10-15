b47508eb08e8d725219ddeff2e5b8be3979e4af7
Matthew Risley <mrisley@Matthews-MacBook-Pro.local>
2018-10-15 17:04:43 -0400

    Notes

--- Markov_Chain_Model.R
+++ Markov_Chain_Model.R
@@ -1,3 +1,14 @@
 print("Hello World")
 
+# Change file path to data 
+setwd("/Users/mrisley/Google Drive File Stream/Team Drives/CMDA Capstone: Cyber Warriors/Data")
+data_1 <-read.csv("MS10PrivData.csv")
+
+#SIR Model
+# Suseptable and Infected Node ->  Suseptable and Infected Node 
+
+#Population and Initial Condition? 
+# Probablity 
+# inital_1 = 100% infected
+# -> next node = 20% prob of getting infected
 
