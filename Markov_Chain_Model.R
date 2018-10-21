aaed5d5e2a625de292de8a98aad0eb0c0c760a04
Matthew Risley <mrisley@Matthews-MacBook-Pro.local>
2018-10-21 18:21:51 -0400

    Bayesian Approach

--- Markov_Chain_Model.R
+++ Markov_Chain_Model.R
@@ -1,10 +1,10 @@
 print("Hello World")
 
 # Change file path to data 
-setwd("/Users/mrisley/Google Drive File Stream/Team Drives/CMDA Capstone: Cyber Warriors/Data")
+setwd("~/Google Drive File Stream/Team Drives/CMDA Capstone: Cyber Warriors/Data")
 data_1 <-read.csv("MS10PrivData.csv")
 
-#SIR Model
+#SIR Model?
 # Suseptable and Infected Node ->  Suseptable and Infected Node 
 
 #Population and Initial Condition? 
@@ -12,3 +12,13 @@ data_1 <-read.csv("MS10PrivData.csv")
 # inital_1 = 100% infected
 # -> next node = 20% prob of getting infected
 
+# Bayesian Statistics Approach
+#source("http://bioconductor.org/biocLite.R")
+#biocLite(c("graph", "RBGL", "Rgraphviz"))
+#install.packages("gRbase", dependencies=TRUE); 
+#install.packages("gRain", dependencies=TRUE); 
+#install.packages("gRim", dependencies=TRUE)
+library(gRain)
+
+data(cad1)
+head(cad1, 5)
